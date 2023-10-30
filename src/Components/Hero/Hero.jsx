5569d5b6b848907a39f2125a9eae630e5fd16dc0
Ajith <ajithmurali499@gmail.com>
2023-10-30 20:49:24 +0530

    Set all in slide bar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Hero.css'
+import {HiLocationMarker} from 'react-icons/hi'
+import CountUp from 'react-countup'
 function Hero() {
   return (
     <div>
@@ -14,12 +16,37 @@ function Hero() {
                    Most Suitable <br/>Property
                    </h1> 
                 </div>
-                <div className=" flexColStart hero-des">
+                <div className=" flexColStart hero-des secondaryText">
                     <span>Find a variety of properties that suit you very easilty</span>
                     <span>Forgot all difficulties in finding a residence for you</span>
                 </div>
-                <div className="search-bar">
-                    search bar
+                <div className="flexCenter search-bar">
+                   <HiLocationMarker color="var(--blue)" size={25}/>
+                   <input type="search"/>
+                   <button className='button'>Search</button>
+                </div>
+                <div className="flexCenter stats">
+                  <div className="flexColStart stat">
+                    <span>
+                      <CountUp start={8800} end={9000} duration={4} />
+                      <span className='pluse'>+</span>
+                    </span>
+                    <span className='secondaryText'>Premium Products</span>
+                  </div>
+                  <div className="flexColStart stat">
+                    <span>
+                      <CountUp start={1} end={2000} duration={4} />
+                      <span className='pluse'>+</span>
+                    </span>
+                    <span className='secondaryText'>Happy Customers</span>
+                  </div>
+                  <div className="flexColStart stat">
+                    <span>
+                      <CountUp  end={28} />
+                      <span className='pluse'>+</span>
+                    </span>
+                    <span className='secondaryText'>Award Winings</span>
+                  </div>
                 </div>
             </div>
         
