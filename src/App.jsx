20963cebadefb3f082318043a954d3663de5af5a
Ajith <ajithmurali499@gmail.com>
2023-10-31 12:43:34 +0530

    blur color set

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from './Components/Header/Header'
 import Hero from "./Components/Hero/Hero";
+import './App.css'
 function App() {
   return (
-    <>
+    <div className="App">
+      <div>
+        <div className="White-gradient" />
     <Header/>
     <Hero/>
-    </>
+    </div>
+    </div>
   );
-}
+} 
 
 export default App;
